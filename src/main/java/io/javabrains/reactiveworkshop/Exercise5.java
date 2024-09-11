fecd4228e6f198ef4ca73da8b76102262c4fe052
OkashaMoosa <okasha.sujela@deliveryhero.com>
2024-09-11 13:52:03 +0200

    Overriding BaseSubscriber class methods and playing with Flux errors and hooks

--- src/main/java/io/javabrains/reactiveworkshop/Exercise5.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise5.java
@@ -1,5 +1,8 @@
 package io.javabrains.reactiveworkshop;
 
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.BaseSubscriber;
+
 import java.io.IOException;
 
 public class Exercise5 {
@@ -10,12 +13,27 @@ public class Exercise5 {
 
         // Subscribe to a flux using the error and completion hooks
         // TODO: Write code here
+        ReactiveSources.intNumbersFlux()
+                .subscribe(x -> System.out.println(x),
+                        err -> System.out.println(err.getMessage()),
+                        () -> System.out.println("Completed!"));
 
         // Subscribe to a flux using an implementation of BaseSubscriber
         // TODO: Write code here
+        MySubscriber<Integer> mySubscriber = new MySubscriber();
+        ReactiveSources.intNumbersFlux()
+                .subscribe(mySubscriber);
 
         System.out.println("Press a key to end");
         System.in.read();
     }
 
+}
+
+class MySubscriber<T> extends BaseSubscriber<T> {
+    @Override
+    protected void hookOnSubscribe(Subscription subscription) {
+        System.out.println("My Subscriber Created");
+        request(1);
+    }
 }
